dd17390fbd5464b46c6e6b6f2f5158440cbee6e6
ColdAlgorithem <68822429+ColdAlgorithem@users.noreply.github.com>
2021-11-30 21:34:21 +0100

    osnova
    dodal sem luči in travo oz. floor

---
+++ 17-game/SceneBuilder.js
@@ -0,0 +1,38 @@
+import { Mesh } from './Mesh.js';
+import { Light } from './Light.js';
+import { Node } from './Node.js';
+import { Model } from './Model.js';
+import { Camera } from './Camera.js';
+import { Floor } from './Floor.js';
+import { Scene } from './Scene.js';
+
+export class SceneBuilder {
+
+    constructor(spec) {
+        this.spec = spec;
+    }
+
+    createNode(spec) {
+        switch (spec.type) {
+            case 'camera': return new Camera(spec);
+            case 'model': {
+                const mesh = new Mesh(this.spec.meshes[spec.mesh]);
+                const texture = this.spec.textures[spec.texture];
+                return new Model(mesh, texture, spec);
+            }
+            case 'floor':{
+                const floor = new Floor(50,50)
+                const floorTexture = this.spec.textures[spec.texture]
+                return new Model(floor,floorTexture,spec)
+            }
+            default: return new Node(spec);
+        }
+    }
+
+    build() {
+        let scene = new Scene();
+        this.spec.nodes.forEach(spec => scene.addNode(this.createNode(spec)));
+        return scene;
+    }
+
+}
